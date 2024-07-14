b18a5d881155c8efedd4cdc3f6c426806842ca09
Girik <girik2001@gmail.com>
2024-07-14 16:15:44 +0530

    update on error signup page errors

--- app.py
+++ app.py
@@ -9,24 +9,25 @@ if 'username' not in st.session_state:
 if 'page' not in st.session_state:
     st.session_state.page = 'login'
 
+def login_or_signup_trigger():
+    if st.session_state.page == 'login':
+        return 'pages/login_page.py'
+    elif st.session_state.page == 'signup':
+        return 'pages/signup_page.py'
 
 
-login_page = st.Page('pages/login_page.py', title='Login', icon=":material/login:")
-sign_up_page = st.Page('pages/signup_page.py', title='Signup', icon=":material/login:")
+
+login_page = st.Page(page='pages/login_page.py', title='Login', icon=":material/login:")
+sign_up_page = st.Page(page='pages/signup_page.py', title='Signup', icon=":material/login:")
 home_page = st.Page('pages/home_page.py', title='Girik Sanghi')
 
 
 
 if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "About Me" : [home_page],
-        }
-    )
-else:
-    if st.session_state.page == 'login':
-        pg = st.navigation([login_page])
-    elif st.session_state.page == 'signup':
-        pg = st.navigation([sign_up_page])
+    pg = st.navigation([home_page])
+elif st.session_state.logged_in!=True and st.session_state.page=='login':
+    pg = st.navigation([login_page])
+elif st.session_state.logged_in!=True and st.session_state.page=='signup':
+    pg = st.navigation([sign_up_page])
 
 pg.run()
